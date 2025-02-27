830c1789b538513bbfc7d3b805fb9061ad6c4562
Dmytro Aksonov <dima.aksenov2411@gmail.com>
2025-02-27 18:09:40 +0200

    fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -29,7 +29,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(target) {
-    if (target instanceof Carnivore) {
+    if (target instanceof Herbivore) {
       return;
     }
 
